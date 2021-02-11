7c601d74db8c6e8833b580205748d2d6936bb640
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-02-11 11:27:31 +0100

    + Wrapper component

--- src/app.styles.ts
+++ src/app.styles.ts
@@ -20,4 +20,37 @@ body {
     box-sizing: border-box;
     font-family: 'Catamaran', sans-serif;
 }
+
+
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > p {
+    color: #fff;
+  }
+
+  .score {
+    color: #222;
+    font-size: 2rem;
+    margin: 0;
+  }
+  h1 {
+    font-family: "Fascinate Inline", sans-serif;
+    background-image: linear-gradient(180deg, #fff, #87f1ff);
+    background-size: 100%;
+    background-clip: text;
+    --webkit-background-clip: text;
+    --webkit-text-fill-color: transparent;
+    --moz-background-clip: text;
+    --moz-text-fill-color: transparent;
+    filter: drop-shadow(2px 2px #0085a3);
+    font-size: 70px;
+    text-align: center;
+    margin: 20px;
+    color: white;
+  }
 `;
