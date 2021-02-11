b3998c7e6bf4ea212c64a468239387dc979b11ae
Timothy Siskin <tymoteuszczyz@gmail.com>
2021-02-11 09:52:59 +0100

    fixed bug in url fetch

--- src/API.ts
+++ src/API.ts
@@ -22,7 +22,7 @@ export const fetchQuizQuestions = async (
   amount: number,
   difficulty: Difficulty
 ) => {
-  const endpoint = `http://opnetdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
   return data.results.map((question: Question) => ({
     ...question,
